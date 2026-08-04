22d005e84732fbee494a4d9a1398fd1be062254e
Mark23-Lozada <markjosephlozada251@gmail.com>
2026-08-04 15:26:49 +0800

    new

--- PAGES/admin_leaves.php
+++ PAGES/admin_leaves.php
@@ -86,6 +86,7 @@ $admin_query = $conn->query("SELECT lr.*, e.full_name, e.department
     <link href="../LIBRARIES/bootstrap.min.css" rel="stylesheet">
     <script src="../LIBRARIES/tailwind.js"></script>
     <script src="../LIBRARIES/sweetalert2.all.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body class="bg-zinc-100 font-sans antialiased h-screen overflow-hidden">
 
